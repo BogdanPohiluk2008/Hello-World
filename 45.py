ed2b461fa5fad5b859b72922ddff973ee1c9070e
BogdanPohiluk2008 <58136449+BogdanPohiluk2008@users.noreply.github.com>
2019-11-24 14:53:26 +0300

    Add files via upload

---
+++ 45.py
@@ -0,0 +1,23 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import time
+mc = minecraft.Minecraft.create()
+mc.setBlocks(50,80,-22,100,110,-22,7)
+mc.setBlocks(100,80,-22,100,110,-52,7)
+mc.setBlocks(100,80,-52,50,110,-52,7)
+#mc.setBlocks(100,80,-52,50,80,-22,7)
+mc.setBlocks(50,81,-25,50,110,-51,7)
+mc.setBlocks(99,82,-49,51,93,-22,0)
+mc.setBlock(99,81,-50,133)
+mc.setBlocks(99,94,-47,51,78,-22,0)
+mc.setBlocks(99,80,-46,51,80,-21,7)
+mc.setBlocks(98,81,-43,52,81,-43,7)
+
+
+
+
+
+
+
+
+
